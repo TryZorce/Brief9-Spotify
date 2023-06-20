12a92787f55dc006c9ac69c99ed8c84741bc9665
TryZorce <christopher.moron@gmail.com>
2023-06-20 14:39:47 +0200

    Initial Commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './header.module.scss';
+
+const Header = () => {
+return (
+<div>
+<p className={styles.red}>Header</p>
+</div>
+);
+};
+
+export default Header;
---
+++ src/app/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './list.module.scss';
+import data from "./data.json";
+
+const List = () => {
+    return (
+
+        <div>
+            <p></p>
+        </div>
+
+    );
+};
+export default List;
